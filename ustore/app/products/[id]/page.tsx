97adc3693012c729c08c24bfafbbff96cc1bdee8
Carlos Daniel <144691269+KarlosNiel@users.noreply.github.com>
2025-06-04 11:57:17 -0300

    agora correção que vai permitir o buildgit add .!

--- ustore/app/products/[id]/page.tsx
+++ ustore/app/products/[id]/page.tsx
@@ -3,10 +3,8 @@ import { HeroCardDetailProduct } from "./_components/HeroCardDetailProduct";
 import { HeroPassPage } from "./_components/HeroPassPage";
 
 
-export default async function ProductPage({ params }: { params: { id: string } } | { params: Promise<{ id: string }> }) {
-    const resolvedParams = await params as { id: string };
-    const { id } = resolvedParams;
-    // ...
+export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
 
 
     // Busca o produto atual
